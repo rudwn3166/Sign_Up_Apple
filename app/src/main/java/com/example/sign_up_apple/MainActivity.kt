00ca47a1be131ad631a6a7290fd6d6fa71e11024
Joo <rudwn3166@naver.com>
2021-08-25 19:51:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sign_up_apple/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sign_up_apple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener {
+
+            val Id=adminId.text.toString()
+            val Pw=adminPw.text.toString()
+            if (Id=="admin@test.com"&& Pw=="qwer"){
+                Toast.makeText(this, "관리자입니다", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
